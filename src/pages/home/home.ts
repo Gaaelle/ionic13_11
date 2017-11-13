8b9ff862979ea3d341e2ed408abad13d1f3324d1
Gaelle <gaaelle.robart@gmail.com>
2017-11-13 14:45:04 +0100

    'filter'

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,23 +7,33 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
   users: Array<Object>;
+  buffer: Array<Object>;
   searchType: 'name' | 'email' = 'name';
   searchValue: string = '';
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, public alertModal: AlertController) {
     this.users = new Array<Object>();
 
     this.users.push({ name: "Nom", email: "email@email.com" });
     this.users.push({ name: "moi", email: "moi@email.com" });
     this.users.push({ name: "toi", email: "toi@email.com" });
 
+    this.buffer = this.users;
+
   }
 
   search() {
-  this.users = this.users.filter( (user) => {
+  this.users = this.buffer.filter( (user) => {
     let name = user[this.searchType].toLowerCase();
-    return name.startsWith(this.searchValue.toLowerCase());
+    return name.startsWith(this.searchValue);
     })
+    if (this.users.length == 0) {
+      this.alertModal.create( {
+      title: 'Hum !',
+      subTitle: 'Aucun utilisateur trouvé',
+      buttons: ['ok']
+      }).present();
+      }
   }
 
 }
