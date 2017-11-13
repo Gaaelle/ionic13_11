8a1d89ceea3ed2befdf398b904266b5268182745
Gaelle <gaaelle.robart@gmail.com>
2017-11-13 12:35:58 +0100

    test button icon

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,8 +6,14 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
+  users: Array<Object>;
 
   constructor(public navCtrl: NavController) {
+    this.users = new Array<Object>();
+
+    this.users.push({ name: "Nom", email: "email@email.com" });
+    this.users.push({ name: "moi", email: "moi@email.com" });
+    this.users.push({ name: "toi", email: "toi@email.com" });
 
   }
 
